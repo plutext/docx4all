7fc7827bacd4ba7afc68be399347abe602b32f50
jojada <jojada@e6e0f55d-d614-4f4a-b1a3-5214db3aa930>
2008-01-14 03:20:07 +0000

    Added getDocumentElements()

--- src/org/docx4all/swing/text/TextSelector.java
+++ src/org/docx4all/swing/text/TextSelector.java
@@ -22,14 +22,12 @@ package org.docx4all.swing.text;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.swing.text.BadLocationException;
+import javax.swing.text.Element;
 
 import org.apache.log4j.Logger;
 import org.docx4all.util.DocUtil;
 import org.docx4all.xml.ElementML;
-import org.docx4all.xml.ObjectFactory;
 import org.docx4all.xml.ParagraphML;
-import org.docx4all.xml.RunContentML;
 
 /**
  *	@author Jojada Tirtowidjojo - 19/12/2007
@@ -37,9 +35,8 @@ import org.docx4all.xml.RunContentML;
 public class TextSelector {
 	private static Logger log = Logger.getLogger(TextSelector.class);
 
-	private final WordMLDocument doc;
-	private final int offset, length;
-	private DocumentElement firstLeaf, lastLeaf;
+	protected final WordMLDocument doc;
+	protected final int offset, length;
 	
 	public TextSelector(WordMLDocument doc, int offset, int length) throws BadSelectionException {
 		select(doc, offset, offset + length);
@@ -48,38 +45,74 @@ public class TextSelector {
 		this.length = length;
 	}
 	
-    private void select(WordMLDocument doc, int p0, int p1) throws BadSelectionException {
+	public List<DocumentElement> getDocumentElements() {
+		DocumentElement root = (DocumentElement) doc.getDefaultRootElement();
+		return getChildren(root);
+	}
+	
+	private List<DocumentElement> getChildren(DocumentElement elem) {
+		List<DocumentElement> theChildren = new ArrayList<DocumentElement>();
+		
+		int startIdx = elem.getElementIndex(offset);
+		int endIdx = elem.getElementIndex(offset + length - 1);
+		
+		while (startIdx <= endIdx) {
+			DocumentElement child = (DocumentElement) elem.getElement(startIdx++);
+			ElementML childML = child.getElementML();
+				
+			if (offset <= child.getStartOffset()
+				&& child.getEndOffset() <= offset + length) {
+				// child is fully selected				
+				if (childML instanceof ParagraphML && childML.isImplied()) {
+					theChildren.addAll(getChildren(child));
+				} else {
+					theChildren.add(child);
+				}
+			} else if (!child.isLeaf()) {
+				//partially selected block
+				theChildren.addAll(getChildren(child));
+				
+			} else {
+				//partially selected leaf
+				theChildren.add(child);
+			}
+		}
+		
+		return theChildren;
+	}
+	
+	private void select(WordMLDocument doc, int p0, int p1) throws BadSelectionException {
     	if (p0 >= p1) {
     		throw new BadSelectionException("Bad Selection", p0, p1-p0);
     	}
     	
-    	this.firstLeaf = (DocumentElement) doc.getCharacterElement(p0);
+    	DocumentElement firstLeaf = (DocumentElement) doc.getCharacterElement(p0);
     	
 		if (log.isDebugEnabled()) {
 			log.debug("select(): [p0, p1] = [" + p0 + ", " + p1 + "]");
-			log.debug("select(): Leaf Element at p0 = " + this.firstLeaf);
+			log.debug("select(): Leaf Element at p0 = " + firstLeaf);
 		}
 		
-		if (this.firstLeaf.getStartOffset() < p0 && !this.firstLeaf.isEditable()) {
-			this.firstLeaf = null;
+		if (firstLeaf.getStartOffset() < p0 && !firstLeaf.isEditable()) {
+			firstLeaf = null;
 			throw new BadSelectionException("Bad Start Position", p0, p1 - p0);
 		}
 
-		this.lastLeaf = (DocumentElement) doc.getCharacterElement(p1);
+		DocumentElement lastLeaf = (DocumentElement) doc.getCharacterElement(p1 - 1);
 
 		if (log.isDebugEnabled()) {
 			log.debug("select(): [p0, p1] = [" + p0 + ", " + p1 + "]");
-			log.debug("select(): Leaf Element at p1 = " + this.lastLeaf);
+			log.debug("select(): Leaf Element at p1 = " + lastLeaf);
 		}
 
-		if (this.lastLeaf.getStartOffset() < p1 && !this.lastLeaf.isEditable()) {
-			this.lastLeaf = null;
+		if (p1 < lastLeaf.getEndOffset() && !lastLeaf.isEditable()) {
+			lastLeaf = null;
 			throw new BadSelectionException("Bad End Position", p0, p1 - p0);
 		}
 
-		if (this.firstLeaf != this.lastLeaf
-			&& (this.firstLeaf.getStartOffset() != p0
-					|| this.lastLeaf.getEndOffset() != p1)) {
+		if (firstLeaf != lastLeaf
+			&& (firstLeaf.getStartOffset() != p0
+					|| lastLeaf.getEndOffset() != p1)) {
 			DocumentElement root = 
 				(DocumentElement) doc.getDefaultRootElement();
 			List<String> path0 = DocUtil.getElementNamePath(root, p0);
@@ -91,6 +124,11 @@ public class TextSelector {
 		}
     }
     
+    protected boolean isFullySelected(Element elem) {
+    	return (offset <= elem.getStartOffset() 
+    			&& elem.getEndOffset() <= offset + length);
+    }
+
 }// TextSelector class
 
 
