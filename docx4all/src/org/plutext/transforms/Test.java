e35daa5e489e51d7c8b4b4098cd11a8326f59a2d
jharrop <jharrop@e6e0f55d-d614-4f4a-b1a3-5214db3aa930>
2008-05-06 01:50:08 +0000

    Put sdt and style elements in w: namespace.

--- docx4all/src/org/plutext/transforms/Test.java
+++ docx4all/src/org/plutext/transforms/Test.java
@@ -1,3 +1,21 @@
+/*
+ *  Copyright 2008, Plutext Pty Ltd.
+ *   
+ *  This file is part of Docx4all.
+
+    Docx4all is free software: you can redistribute it and/or modify
+    it under the terms of version 3 of the GNU General Public License 
+    as published by the Free Software Foundation.
+
+    Docx4all is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License   
+    along with Docx4all.  If not, see <http://www.gnu.org/licenses/>.
+    
+ */
 package org.plutext.transforms;
 
 import javax.xml.bind.JAXBException;
@@ -23,7 +41,6 @@ public class Test {
 			Unmarshaller u = Context.jcTransforms.createUnmarshaller();
 			u.setEventHandler(new org.docx4j.jaxb.JaxbValidationEventHandler());					
 			transformsObj = (org.plutext.transforms.Transforms)u.unmarshal( new java.io.File(filename) );
-			System.out.println("Success!");
 			
 			// Manipulate
 			for (Transforms.T t : transformsObj.t) {
