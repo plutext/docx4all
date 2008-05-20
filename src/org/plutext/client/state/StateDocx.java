e2f621f8ae0b256c404b492aee2ce694b0071b8f
jojada <jojada@e6e0f55d-d614-4f4a-b1a3-5214db3aa930>
2008-05-20 03:52:43 +0000

    Refactored constructor.

--- src/org/plutext/client/state/StateDocx.java
+++ src/org/plutext/client/state/StateDocx.java
@@ -19,188 +19,250 @@
 
 package org.plutext.client.state;
 
-import org.apache.log4j.Logger;
+import java.math.BigInteger;
 import java.util.HashMap;
+import java.util.Map;
 
-import org.plutext.client.wrappedTransforms.TransformAbstract;
-import org.docx4j.wml.SdtBlock;
+import org.apache.log4j.Logger;
+import org.docx4all.swing.WordMLTextPane;
+import org.docx4all.swing.text.DocumentElement;
+import org.docx4all.swing.text.WordMLDocument;
+import org.docx4all.xml.DocumentML;
 import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
-import org.plutext.client.Util;
-import org.plutext.client.CustomProperties;
 import org.docx4j.wml.Id;
+import org.docx4j.wml.SdtBlock;
+import org.plutext.client.CustomProperties;
+import org.plutext.client.Util;
+import org.plutext.client.wrappedTransforms.TransformAbstract;
 
 /* Represent the configuration of the
-     * document, and certain transient
-     * document-level state information.
-     */ 
-    public class StateDocx
-    {
+ * document, and certain transient
+ * document-level state information.
+ */
+public class StateDocx {
 
-    	private static Logger log = Logger.getLogger(StateDocx.class);
+	private static Logger log = Logger.getLogger(StateDocx.class);
+
+	public StateDocx(WordMLTextPane textPane) {
+		WordMLDocument doc = (WordMLDocument) textPane.getDocument();
+		String fileUri = (String) doc.getProperty(WordMLDocument.FILE_PATH_PROPERTY);
+		if (fileUri == null) {
+			throw new IllegalArgumentException("Missing WordMLDocument.FILE_PATH_PROPERTY");
+		}
+
+    	int idx = fileUri.indexOf("/alfresco/");
+    	if (idx <= 0) {
+    		//temporary checking
+    		//TODO: Has to check whether fileUri's protocol is webdav
+    		//and its context is correct.
+    		throw new IllegalArgumentException("Invalid WordMLDocument.FILE_PATH_PROPERTY value");
+    	}
     	
-        public StateDocx(WordprocessingMLPackage wordMLPackage)
-        {
-            log.debug("StateDocx constructor fired");
+		this.docID = fileUri.substring(idx);
+    	init(doc);		
+	}
 
-            uptodate = true;
-            
-            try
-            {
+	private void init(WordMLDocument doc) {
+  		DocumentElement root = (DocumentElement) doc.getDefaultRootElement();
+    	WordprocessingMLPackage wordMLPackage = 
+    		((DocumentML) root.getElementML()).getWordprocessingMLPackage();
 
-// TODO: implement!            	
-//                foreach (SdtBlock sdt in Globals.ThisAddIn.Application.ActiveDocument.ContentControls)
-//                {
-//                    // Record the known state of each content control
-//                    controlMap.Add(sdt);
-//                }
+    	
+		this.uptodate = true;
+		
+		this.tSequenceNumberAtLoadTime = Integer.parseInt(Util
+				.getCustomDocumentProperty(wordMLPackage
+						.getDocPropsCustomPart(),
+						CustomProperties.DOCUMENT_TRANSFORM_SEQUENCENUMBER));
 
-                contentControlSnapshots = new HashMap<Id, ContentControlSnapshot>();
-                
-                // TODO - contentControlSnapshots.put a ContentControlSnapshot of each sdt
+		this.tSequenceNumberApplied = this.tSequenceNumberAtLoadTime;
 
-            	
-            } 
-            catch (Exception ex)
-            {
-                ex.printStackTrace();
-            }
+		this.tSequenceNumberHighestFetched = this.tSequenceNumberAtLoadTime;
 
-            tSequenceNumberAtLoadTime  
-            	= Integer.parseInt(Util.getCustomDocumentProperty(
-            			wordMLPackage.getDocPropsCustomPart(), CustomProperties.DOCUMENT_TRANSFORM_SEQUENCENUMBER));
-
-            tSequenceNumberApplied = tSequenceNumberAtLoadTime;
-
-            tSequenceNumberHighestFetched = tSequenceNumberAtLoadTime;
-            
-            org.docx4j.wml.Styles docxStyles = (org.docx4j.wml.Styles)wordMLPackage.getMainDocumentPart().getStyleDefinitionsPart().getJaxbElement();                        
-            stylesSnapshot = new StylesSnapshot(docxStyles );
-
-        }
-
-        private int tSequenceNumberAtLoadTime;
-
-        private int tSequenceNumberApplied;
-		public int getTSequenceNumberApplied() {
-			return tSequenceNumberApplied;
-		}
-		public void setTSequenceNumberApplied(int sequenceNumberApplied) {
-			tSequenceNumberApplied = sequenceNumberApplied;
-		}
-
-        private int tSequenceNumberHighestFetched; 
-		public int getTSequenceNumberHighestFetched() {
-			return tSequenceNumberHighestFetched;
-		}
-		public void setTSequenceNumberHighestFetched(int sequenceNumberHighestFetched) {
-			tSequenceNumberHighestFetched = sequenceNumberHighestFetched;
-		}
-
-        private String docID = null;
-		public String getDocID() {
-			return docID;
-		}
-		public void setDocID(String docID) {
-			this.docID = docID;
-		}
-
-        // These three variables are read from document properties
-
-        private Boolean promptForCheckinMessage;
-		public Boolean getPromptForCheckinMessage() {
-			return promptForCheckinMessage;
-		}
-		public void setPromptForCheckinMessage(Boolean promptForCheckinMessage) {
-			this.promptForCheckinMessage = promptForCheckinMessage;
-		}
-
-        private String chunkingStrategy;
-		public String getChunkingStrategy() {
-			return chunkingStrategy;
-		}
-		public void setChunkingStrategy(String chunkingStrategy) {
-			this.chunkingStrategy = chunkingStrategy;
-		}
-
-
-        private SdtBlock currentCC = null;
-		public SdtBlock getCurrentCC() {
-			return currentCC;
-		}
-		public void setCurrentCC(SdtBlock currentCC) {
-			this.currentCC = currentCC;
+		if (Util.getCustomDocumentProperty(
+				wordMLPackage.getDocPropsCustomPart(),
+				CustomProperties.PROMPT_FOR_CHECKIN_MESSAGE).equals("true")) {
+			this.promptForCheckinMessage = Boolean.TRUE;
 			
-			if (currentCC ==null) {
-				return;
+		} else if (Util.getCustomDocumentProperty(
+				wordMLPackage.getDocPropsCustomPart(),
+				CustomProperties.PROMPT_FOR_CHECKIN_MESSAGE).equals("false")) {
+			this.promptForCheckinMessage = Boolean.FALSE;
+			
+		} else {
+			log.warn(CustomProperties.PROMPT_FOR_CHECKIN_MESSAGE + "unknown");
+			this.promptForCheckinMessage = Boolean.FALSE;
+		}
+		
+		// Expected values: EachBlock, Heading1
+		this.chunkingStrategy = Util.getCustomDocumentProperty(
+				wordMLPackage.getDocPropsCustomPart(),
+				CustomProperties.CHUNKING_STRATEGY);
+
+		initStylesSnapshot(wordMLPackage);
+		initContentControlSnapshots(doc);
+	}
+	
+	private void initStylesSnapshot(WordprocessingMLPackage wordMLPackage) {
+		org.docx4j.wml.Styles docxStyles = 
+			(org.docx4j.wml.Styles) wordMLPackage.getMainDocumentPart().
+				getStyleDefinitionsPart().getJaxbElement();
+		this.stylesSnapshot = new StylesSnapshot(docxStyles);
+	}
+	
+	private void initContentControlSnapshots(WordMLDocument doc) {
+		Map<BigInteger, SdtBlock> map = doc.getSnapshots(0, doc.getLength());
+		if (map != null) {
+			this.contentControlSnapshots = 
+				new HashMap<Id, ContentControlSnapshot>(map.size());
+			
+			for (SdtBlock sdt:map.values()) {
+				ContentControlSnapshot ccs = 
+					new ContentControlSnapshot(sdt);
+				this.contentControlSnapshots.put(ccs.getId(), ccs);
 			}
-			
-			// Looks like this method is only called when
-			// entering a content control?
-			
-				// No, as at May 9, we're also calling it
-				// when exiting (at beginning and end of exit)
-			
-			// Therefore content control already exists in
-			// document, so you can assume it is already 
-			// listed in contentControlSnapshots
-			
-			// But could check, just to make sure?
-			
-			ContentControlSnapshot ccs = (ContentControlSnapshot)contentControlSnapshots.get(
-												currentCC.getSdtPr().getId());
-			if (ccs==null) {
-				ccs = new ContentControlSnapshot(currentCC);								
-			} else {
-				ccs.refresh();
-			}
-			contentControlSnapshots.put(currentCC.getSdtPr().getId(), ccs);
+		} else {
+			this.contentControlSnapshots = 
+				new HashMap<Id, ContentControlSnapshot>();
+		}
+	}
+	
+	private int tSequenceNumberAtLoadTime;
+
+	private int tSequenceNumberApplied;
+
+	public int getTSequenceNumberApplied() {
+		return tSequenceNumberApplied;
+	}
+
+	public void setTSequenceNumberApplied(int sequenceNumberApplied) {
+		tSequenceNumberApplied = sequenceNumberApplied;
+	}
+
+	private int tSequenceNumberHighestFetched;
+
+	public int getTSequenceNumberHighestFetched() {
+		return tSequenceNumberHighestFetched;
+	}
+
+	public void setTSequenceNumberHighestFetched(
+			int sequenceNumberHighestFetched) {
+		tSequenceNumberHighestFetched = sequenceNumberHighestFetched;
+	}
+
+	private String docID = null;
+
+	public String getDocID() {
+		return docID;
+	}
+
+	public void setDocID(String docID) {
+		this.docID = docID;
+	}
+
+	// These three variables are read from document properties
+
+	private Boolean promptForCheckinMessage;
+
+	public Boolean getPromptForCheckinMessage() {
+		return promptForCheckinMessage;
+	}
+
+	public void setPromptForCheckinMessage(Boolean promptForCheckinMessage) {
+		this.promptForCheckinMessage = promptForCheckinMessage;
+	}
+
+	private String chunkingStrategy;
+
+	public String getChunkingStrategy() {
+		return chunkingStrategy;
+	}
+
+	public void setChunkingStrategy(String chunkingStrategy) {
+		this.chunkingStrategy = chunkingStrategy;
+	}
+
+	private SdtBlock currentCC = null;
+
+	public SdtBlock getCurrentCC() {
+		return currentCC;
+	}
+
+	public void setCurrentCC(SdtBlock currentCC) {
+		this.currentCC = currentCC;
+
+		if (currentCC == null) {
+			return;
 		}
 
+		// Looks like this method is only called when
+		// entering a content control?
 
-        private Boolean initialized = false;
+		// No, as at May 9, we're also calling it
+		// when exiting (at beginning and end of exit)
 
+		// Therefore content control already exists in
+		// document, so you can assume it is already 
+		// listed in contentControlSnapshots
 
-        StylesSnapshot stylesSnapshot = null;
-		public StylesSnapshot getStylesSnapshot() {
-			return stylesSnapshot;
+		// But could check, just to make sure?
+
+		ContentControlSnapshot ccs = (ContentControlSnapshot) contentControlSnapshots
+				.get(currentCC.getSdtPr().getId());
+		if (ccs == null) {
+			ccs = new ContentControlSnapshot(currentCC);
+		} else {
+			ccs.refresh(currentCC);
 		}
+		contentControlSnapshots.put(currentCC.getSdtPr().getId(), ccs);
+	}
 
-        /* Maintain an ordered list of controls 
-         * (unless docx4all already has this somewhere else?) */
-        Controls controlMap = new Controls();
-        public Controls getControlMap() {
-        	return controlMap; 
-        }
+	private Boolean initialized = false;
 
-        /* Maintain a hashmap (dictionary) of ContentControlSnapshot wrapped Content Controls 
-         * keyed by ID, so we can detect when one has been updated (even if not
-         * entered)  */
-        HashMap<Id, ContentControlSnapshot> contentControlSnapshots = null;
-         
-//		public void setControlMap(Controls controlMap) {
-//			this.controlMap = controlMap;
-//		}
-        public HashMap<Id, ContentControlSnapshot> getContentControlSnapshots() {
-        	return contentControlSnapshots; 
-        }
+	StylesSnapshot stylesSnapshot = null;
 
-        /* Maintain a collection of Transforms keyed by tSequenceNumber, so we 
-         * can keep track of which ones have been applied.  */
-        	// TODO - rename to wrappedTransforms
-        HashMap<Integer, TransformAbstract> wrappedTransforms 
-        	= new HashMap<Integer, TransformAbstract>();
-		public HashMap<Integer, TransformAbstract> getWrappedTransforms() {
-			return wrappedTransforms;
-		}
+	public StylesSnapshot getStylesSnapshot() {
+		return stylesSnapshot;
+	}
 
+	/* Maintain an ordered list of controls 
+	 * (unless docx4all already has this somewhere else?) */
+	Controls controlMap = new Controls();
 
-        //Boolean uptodate = true;
-        public boolean uptodate;
-		public boolean getUptodate() {
-			return (tSequenceNumberApplied == tSequenceNumberHighestFetched);
-		}
-		public void setUptodate(boolean uptodate) {
-			this.uptodate = uptodate;
-		}
+	public Controls getControlMap() {
+		return controlMap;
+	}
 
-    }
+	/* Maintain a hashmap (dictionary) of ContentControlSnapshot wrapped Content Controls 
+	 * keyed by ID, so we can detect when one has been updated (even if not
+	 * entered)  */
+	HashMap<Id, ContentControlSnapshot> contentControlSnapshots = null;
+
+	//		public void setControlMap(Controls controlMap) {
+	//			this.controlMap = controlMap;
+	//		}
+	public HashMap<Id, ContentControlSnapshot> getContentControlSnapshots() {
+		return contentControlSnapshots;
+	}
+
+	/* Maintain a collection of Transforms keyed by tSequenceNumber, so we 
+	 * can keep track of which ones have been applied.  */
+	// TODO - rename to wrappedTransforms
+	HashMap<Integer, TransformAbstract> wrappedTransforms = new HashMap<Integer, TransformAbstract>();
+
+	public HashMap<Integer, TransformAbstract> getWrappedTransforms() {
+		return wrappedTransforms;
+	}
+
+	//Boolean uptodate = true;
+	public boolean uptodate;
+
+	public boolean getUptodate() {
+		return (tSequenceNumberApplied == tSequenceNumberHighestFetched);
+	}
+
+	public void setUptodate(boolean uptodate) {
+		this.uptodate = uptodate;
+	}
+
+}
