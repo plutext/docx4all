271cb4b17cd3fd9020588d26b15c74c79b5ed838
jojada <jojada@e6e0f55d-d614-4f4a-b1a3-5214db3aa930>
2008-06-20 03:21:42 +0000

    RunInsML class

---
+++ src/main/java/org/docx4all/xml/RunInsML.java
@@ -0,0 +1,183 @@
+/*
+ *  Copyright 2007, Plutext Pty Ltd.
+ *   
+ *  This file is part of Docx4all.
+
+    Docx4all is free software: you can redistribute it and/or modify
+    it under the terms of version 3 of the GNU General Public License 
+    as published by the Free Software Foundation.
+
+    Docx4all is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License   
+    along with Docx4all.  If not, see <http://www.gnu.org/licenses/>.
+    
+ */
+
+package org.docx4all.xml;
+
+import java.awt.Color;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
+import javax.xml.datatype.XMLGregorianCalendar;
+
+import org.docx4j.XmlUtils;
+
+
+/**
+ *	@author Jojada Tirtowidjojo - 17/06/2008
+ */
+public class RunInsML extends ElementML {
+	
+	public RunInsML(Object docxObject) {
+		this(docxObject, false);
+	}
+	
+	public RunInsML(Object docxObject, boolean isDummy) {
+		super(docxObject, isDummy);
+	}
+	
+	public BigInteger getId() {
+		return ((org.docx4j.wml.RunIns) this.docxObject).getId();
+	}
+	
+	public void setId(BigInteger id) {
+		((org.docx4j.wml.RunIns) this.docxObject).setId(id);
+	}
+	
+	public XMLGregorianCalendar getDate() {
+		return ((org.docx4j.wml.RunIns) this.docxObject).getDate();
+	}
+	
+	public void setDate(XMLGregorianCalendar date) {
+		((org.docx4j.wml.RunIns) this.docxObject).setDate(date);
+	}
+	
+	public boolean canAddSibling(ElementML elem, boolean after) {
+		boolean canAdd = false;
+		
+		if (elem instanceof RunML || elem instanceof RunDelML || elem instanceof RunInsML) {
+			canAdd = super.canAddSibling(elem, after);
+		}
+		
+		return canAdd;
+	}
+	
+	public Object clone() {
+		Object obj = null;
+		if (this.docxObject != null) {
+			obj = XmlUtils.deepCopy(this.docxObject);
+		}
+		
+		return new RunInsML(obj, this.isDummy);
+	}
+	
+	public boolean canAddChild(int idx, ElementML child) {
+		boolean canAdd = true;
+		
+		if (!(child instanceof RunML)) {
+			canAdd = false;
+		} else {
+			canAdd = super.canAddChild(idx, child);
+		}
+		
+		return canAdd;
+	}
+	
+	public void addChild(int idx, ElementML child, boolean adopt) {
+		if (!(child instanceof RunML)) {
+			throw new IllegalArgumentException("NOT a RunML");
+		}
+		if (child.getParent() != null) {
+			throw new IllegalArgumentException("Not an orphan.");
+		}
+		super.addChild(idx, child, adopt);
+	}
+		
+	public void setParent(ElementML parent) {
+		if (parent != null && !(parent instanceof ParagraphML)) {
+			throw new IllegalArgumentException("NOT a ParagraphML.");
+		}
+		this.parent = parent;
+	}
+	
+	protected List<Object> getDocxChildren() {
+		List<Object> theChildren = null;
+		
+		if (this.docxObject instanceof org.docx4j.wml.RunIns) {
+			theChildren = ((org.docx4j.wml.RunIns) this.docxObject).getCustomXmlOrSmartTagOrSdt();
+		}
+		
+		return theChildren;
+	}
+	
+	protected void init(Object docxObject) {
+		org.docx4j.wml.RunIns runIns = null;
+		
+		if (docxObject == null) {
+			;// implied ParagraphML
+			
+		} else if (docxObject instanceof org.docx4j.wml.RunIns) {
+			runIns = (org.docx4j.wml.RunIns) docxObject;
+			this.isDummy = false;
+			
+		} else {
+			throw new IllegalArgumentException("Unsupported Docx Object = "
+					+ docxObject);
+		}
+
+		initChildren(runIns);
+	}
+	
+	private void initChildren(org.docx4j.wml.RunIns ins) {
+		this.children = null;
+		
+		if (ins == null) {
+			return;
+		}
+		
+		List<Object> pKids = ins.getCustomXmlOrSmartTagOrSdt();
+		if (!pKids.isEmpty()) {
+			SimpleAttributeSet underline = new SimpleAttributeSet();
+			StyleConstants.setUnderline(underline, true);
+			StyleConstants.setForeground(underline, Color.RED);
+
+			this.children = new ArrayList<ElementML>(pKids.size());
+			for (Object o : pKids) {
+				RunML run = new RunML(o, this.isDummy);
+				
+				run.addAttributes(underline, false);
+				run.setParent(RunInsML.this);
+				this.children.add(run);				
+			}
+		}
+	}// initChildren()
+	
+
+}// RunInsML class
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
