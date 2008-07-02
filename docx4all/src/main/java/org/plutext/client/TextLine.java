5be0faebe20ef79729da6aad7f036b6a4dca1f77
jojada <jojada@e6e0f55d-d614-4f4a-b1a3-5214db3aa930>
2008-07-02 15:25:12 +0000

    Preliminary conversion

---
+++ docx4all/src/main/java/org/plutext/client/TextLine.java
@@ -0,0 +1,46 @@
+/*
+ *  Copyright 2007, Plutext Pty Ltd.
+ *   
+ *  This file is part of Docx4all.
+
+    Docx4all is free software: you can redistribute it and/or modify
+    it under the terms of version 3 of the GNU General Public License 
+    as published by the Free Software Foundation.
+
+    Docx4all is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License   
+    along with Docx4all.  If not, see <http://www.gnu.org/licenses/>.
+    
+ */
+
+package org.plutext.client;
+
+
+/**
+ * @author Jojada Tirtowidjojo - 03/07/2008
+ */
+public class TextLine implements Comparable<TextLine> {
+	private String _line;
+	private int _hash;
+
+	public TextLine(String str) {
+		_line = str.replace("\t", "    ");
+		_hash = str.hashCode();
+	}
+	
+	public String getLine() {
+		return _line;
+	}
+
+	public int compareTo(TextLine obj) {
+		Integer thisHash = Integer.valueOf(_hash);
+		Integer objHash = Integer.valueOf(obj._hash);
+		return thisHash.compareTo(objHash);
+	}
+
+}// TextLine class
+
