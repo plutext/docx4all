9124e815b75463185505f5ba28b57bb10eaae361
jojada <jojada@e6e0f55d-d614-4f4a-b1a3-5214db3aa930>
2008-10-23 12:21:12 +0000

    Moved Content Control history view and Recent Changes view related menus from 'View' men u into 'Plutext' menu.

--- docx4all/src/main/java/org/docx4all/ui/menu/PlutextMenu.java
+++ docx4all/src/main/java/org/docx4all/ui/menu/PlutextMenu.java
@@ -19,9 +19,13 @@
 
 package org.docx4all.ui.menu;
 
+import java.awt.Cursor;
+
 import javax.swing.JInternalFrame;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.SwingUtilities;
+import javax.swing.text.Element;
 
 import net.sf.vfsjfilechooser.utils.VFSUtils;
 
@@ -33,6 +37,8 @@ import org.docx4all.swing.WordMLTextPane;
 import org.docx4all.swing.text.WordMLDocument;
 import org.docx4all.ui.main.ToolBarStates;
 import org.docx4all.ui.main.WordMLEditor;
+import org.docx4all.ui.menu.ViewMenu.CloseViewEnabler;
+import org.docx4all.ui.menu.ViewMenu.OpenViewEnabler;
 import org.jdesktop.application.Action;
 import org.jdesktop.application.ResourceMap;
 import org.plutext.client.Mediator;
@@ -56,9 +62,9 @@ public class PlutextMenu extends UIMenu {
 	// ==========
 	// Used as an argument to JMenu.setName().
 	// Therefore it can be used in .properties file
-	// to configure Team Menu property in the menu bar
+	// to configure Plutext Menu property in the menu bar
 	/**
-	 * The name of JMenu object that hosts Team menu in the menu bar
+	 * The name of JMenu object that hosts Plutext menu in the menu bar
 	 */
 	public final static String PLUTEXT_MENU_NAME = "plutextMenu";
 
@@ -72,16 +78,42 @@ public class PlutextMenu extends UIMenu {
 	// Swing Application Framework
 
 	/**
-	 * The action name of 'Fetch Remote Edits' Team menu
+	 * The action name of 'View Content Control History' menu
+	 */
+	public final static String VIEW_CONTENT_CONTROL_HISTORY_ACTION_NAME = "viewContentControlHistory";
+	
+	/**
+	 * The action name of 'View Recent Changes' menu
+	 */
+	public final static String VIEW_RECENT_CHANGES_ACTION_NAME = "viewRecentChanges";
+	
+	/**
+	 * The action name of 'Close History View' menu
+	 */
+	public final static String CLOSE_CONTENT_CONTROL_HISTORY_VIEW_ACTION_NAME = "closeContentControlHistoryView";
+	
+	/**
+	 * The action name of 'Close Recent Changes View' menu
+	 */
+	public final static String CLOSE_RECENT_CHANGES_VIEW_ACTION_NAME = "closeRecentChangesView";
+	
+	/**
+	 * The action name of 'Fetch Remote Edits' menu
 	 */
 	public final static String FETCH_REMOTE_EDITS_ACTION_NAME = "fetchRemoteEdits";
 
 	/**
-	 * The action name of 'Commit Local Edits' Team menu
+	 * The action name of 'Commit Local Edits' menu
 	 */
 	public final static String COMMIT_LOCAL_EDITS_ACTION_NAME = "commitLocalEdits";
 
 	private static final String[] _menuItemActionNames = {
+		VIEW_CONTENT_CONTROL_HISTORY_ACTION_NAME,
+		VIEW_RECENT_CHANGES_ACTION_NAME,
+		SEPARATOR_CODE,
+		CLOSE_CONTENT_CONTROL_HISTORY_VIEW_ACTION_NAME,
+		CLOSE_RECENT_CHANGES_VIEW_ACTION_NAME,
+		SEPARATOR_CODE,
 		FETCH_REMOTE_EDITS_ACTION_NAME,
 		COMMIT_LOCAL_EDITS_ACTION_NAME
 	};
@@ -110,7 +142,31 @@ public class PlutextMenu extends UIMenu {
 		WordMLEditor editor = WordMLEditor.getInstance(WordMLEditor.class);
 		ToolBarStates toolbarStates = editor.getToolbarStates();
 		
-		if (FETCH_REMOTE_EDITS_ACTION_NAME.equals(actionName)) {
+    	if (VIEW_CONTENT_CONTROL_HISTORY_ACTION_NAME.equals(actionName)) {
+    		theItem.setEnabled(false);
+    		toolbarStates.addPropertyChangeListener(
+    			ToolBarStates.CURRENT_EDITOR_PROPERTY_NAME,
+    			new OpenViewEnabler(theItem, editor.getContentControlHistoryViewTabTitle()));
+    	
+    	} else if (VIEW_RECENT_CHANGES_ACTION_NAME.equals(actionName)) {
+    		theItem.setEnabled(false);
+    		toolbarStates.addPropertyChangeListener(
+    			ToolBarStates.CURRENT_EDITOR_PROPERTY_NAME,
+    			new OpenViewEnabler(theItem, editor.getRecentChangesViewTabTitle()));
+    		
+        } else if (CLOSE_CONTENT_CONTROL_HISTORY_VIEW_ACTION_NAME.equals(actionName)) {
+    		theItem.setEnabled(false);
+    		toolbarStates.addPropertyChangeListener(
+        			ToolBarStates.CURRENT_EDITOR_PROPERTY_NAME,
+        			new CloseViewEnabler(theItem, editor.getContentControlHistoryViewTabTitle()));
+    		
+        } else if (CLOSE_RECENT_CHANGES_VIEW_ACTION_NAME.equals(actionName)) {
+    		theItem.setEnabled(false);
+    		toolbarStates.addPropertyChangeListener(
+        			ToolBarStates.CURRENT_EDITOR_PROPERTY_NAME,
+        			new CloseViewEnabler(theItem, editor.getRecentChangesViewTabTitle()));
+    		    	
+        } else if (FETCH_REMOTE_EDITS_ACTION_NAME.equals(actionName)) {
     		theItem.setEnabled(false);
     		toolbarStates.addPropertyChangeListener(
     				ToolBarStates.DOC_SHARED_PROPERTY_NAME, 
@@ -125,6 +181,55 @@ public class PlutextMenu extends UIMenu {
 		return theItem;
     }
     
+	@Action public void viewContentControlHistory() {
+        WordMLEditor editor = WordMLEditor.getInstance(WordMLEditor.class);
+        WordMLTextPane editorView =
+        	(WordMLTextPane) editor.getView(editor.getEditorViewTabTitle());
+        if (editorView == null) {
+        	return;
+        }
+        
+        int pos = editorView.getCaretPosition();
+        WordMLDocument doc = (WordMLDocument) editorView.getDocument();
+        Element sdt = null;
+        try {
+        	doc.readLock();
+        	
+        	sdt = doc.getSdtBlockMLElement(pos);
+        } finally {
+        	doc.readUnlock();
+        }
+        if (sdt != null) {
+        	String title = editor.getContentControlHistoryViewTabTitle();
+        	editor.showViewInTab(title);
+        } else {
+	    	ResourceMap rm = editor.getContext().getResourceMap(getClass());
+	        String title = 
+	        	rm.getString(
+	        		PlutextMenu.VIEW_CONTENT_CONTROL_HISTORY_ACTION_NAME + ".Action.text");
+	        String message =
+	        	rm.getString(
+	        		PlutextMenu.VIEW_CONTENT_CONTROL_HISTORY_ACTION_NAME + ".Action.selectSdtMessage");
+	    	editor.showMessageDialog(title, message, JOptionPane.INFORMATION_MESSAGE);
+        }
+        
+	}
+	
+	@Action public void viewRecentChanges() {
+        WordMLEditor editor = WordMLEditor.getInstance(WordMLEditor.class);
+        editor.showViewInTab(editor.getRecentChangesViewTabTitle());
+	}
+	
+	@Action public void closeContentControlHistoryView() {
+        WordMLEditor editor = WordMLEditor.getInstance(WordMLEditor.class);
+        editor.closeViewTab(editor.getContentControlHistoryViewTabTitle());
+	}
+	
+	@Action public void closeRecentChangesView() {
+        WordMLEditor editor = WordMLEditor.getInstance(WordMLEditor.class);
+        editor.closeViewTab(editor.getRecentChangesViewTabTitle());
+	}
+	
 	@Action public void fetchRemoteEdits() {		
 		log.debug("fetchRemoteEdits(): Starting...");
 		
