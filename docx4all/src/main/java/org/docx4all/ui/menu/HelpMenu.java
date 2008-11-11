871854a3c2f3e5513269e7b38285751c2cbb4e80
jharrop <jharrop@e6e0f55d-d614-4f4a-b1a3-5214db3aa930>
2008-11-11 13:19:31 +0000

    v1.0 alpha 2 (uses webdavclient4j vfs 0.9.3, patched VFSJFileChooser, and docx4j 2.1.0)

--- docx4all/src/main/java/org/docx4all/ui/menu/HelpMenu.java
+++ docx4all/src/main/java/org/docx4all/ui/menu/HelpMenu.java
@@ -94,8 +94,8 @@ public class HelpMenu extends UIMenu {
     	
     	String message = 
     		"Docx4all is an open source docx-based word processor, for Windows, OSX, Linux and anywhere else J2SE 6 runs."
-    		+ "\nThis is an early proof of concept release, not yet intended for production use. "
-    		+ "\nFor support and to provide feedback, please visit http://dev.plutext.org/forums/ or our wiki. Contributions welcome - download our developer's appliance to get started quickly."    		
+    		+ "\nThis is an alpha release, not yet intended for production use. "
+    		+ "\nFor support and to provide feedback, please visit http://dev.plutext.org/forums/ or our wiki. Contributions welcome."    		
     		+ "\ndocx4all was programmed by Jojada Tirtowidjojo (lead developer). [Your name here?] "
     		+ "\nDocx4all is free software: you can redistribute it and/or modify it under the terms of v3 of the GNU GPL as published by the FSF."
     		+ "\nCopyright (c) Plutext Pty. Ltd., 2007-2008";
