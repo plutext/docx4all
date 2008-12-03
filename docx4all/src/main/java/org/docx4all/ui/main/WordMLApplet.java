43d362af9d0a0405614b8fbe835bb25dc3a12fcc
jojada <jojada@e6e0f55d-d614-4f4a-b1a3-5214db3aa930>
2008-12-03 13:44:51 +0000

    Cycles over known user credentials and present authentication challenge if needed when opening .docx file specified in 'open_file_url' parameter.

--- docx4all/src/main/java/org/docx4all/ui/main/WordMLApplet.java
+++ docx4all/src/main/java/org/docx4all/ui/main/WordMLApplet.java
@@ -19,21 +19,21 @@
 
 package org.docx4all.ui.main;
 
+import java.util.List;
 import java.util.prefs.Preferences;
 
 import javax.swing.JApplet;
 import javax.swing.JOptionPane;
 import javax.swing.SwingUtilities;
 
-import net.sf.vfsjfilechooser.accessories.bookmarks.Bookmarks;
-import net.sf.vfsjfilechooser.accessories.bookmarks.TitledURLEntry;
 import net.sf.vfsjfilechooser.utils.VFSURIParser;
+import net.sf.vfsjfilechooser.utils.VFSUtils;
 
 import org.apache.commons.vfs.FileObject;
 import org.apache.commons.vfs.FileSystemException;
 import org.apache.log4j.Logger;
 import org.docx4all.ui.menu.FileMenu;
-import org.docx4all.ui.menu.HyperlinkMenu;
+import org.docx4all.util.AuthenticationUtil;
 import org.jdesktop.application.ResourceMap;
 
 /**
@@ -55,17 +55,29 @@ public class WordMLApplet extends JApplet {
         s = getParameter(s);
         if (s == null) {
         	;//pass
-        } else if (s.endsWith(".docx")
-        			&& (s.startsWith("http://")
-        					|| s.startsWith("webdav://"))) {
-        	final String urlParam = s;
-        	SwingUtilities.invokeLater(new Runnable() {
-        		public void run() {
-                	openFile(editor, urlParam);
-        		}
-        	});
+        } else if (s.endsWith(".docx")) {
+        	if (s.startsWith("file://")) {
+        		final String urlParam = s;
+        		SwingUtilities.invokeLater(new Runnable() {
+        			public void run() {
+                        openLocalFile(editor, urlParam);
+        			}
+        		});
+                
+        	} else if (s.startsWith("http://")
+        				|| s.startsWith("webdav://")) {
+        		final String urlParam = s;
+        		SwingUtilities.invokeLater(new Runnable() {
+        			public void run() {
+        				openRemoteFile(editor, urlParam);
+        			}
+        		});
+                
+        	} else {
+            	log.error("Unknown protocol scheme. File = " + s);        		
+        	}
         } else {
-        	log.error("Unknown file type or protocol");
+        	log.error("Unknown file type. File = " + s);        	
         }
 	}
 	
@@ -81,34 +93,26 @@ public class WordMLApplet extends JApplet {
 		log.info("Applet being destroyed.");	
 	}
 	
-	private void openFile(WordMLEditor editor, String urlParam) {
-		if (urlParam.startsWith("http://")) {
-			urlParam = "webdav://" + urlParam.substring(7);
-		}
-		
+	private void openLocalFile(WordMLEditor editor, String urlParam) {
         ResourceMap rm = editor.getContext().getResourceMap(WordMLEditor.class);
         
-		String temp = 
-			rm.getString(Constants.VFSJFILECHOOSER_DEFAULT_WEBDAV_FOLDER_BOOKMARK_NAME);
-		if (temp == null || temp.length() == 0) {
-			temp = "Default Webdav Folder";
-		} else {
-			temp = temp.trim();
-		}
-		temp = createVFSUrl(urlParam, temp);
-		
+	    String localFileUrl = urlParam;
+	    if (System.getProperty("os.name").toLowerCase().startsWith("windows")) {
+	    	if (urlParam.startsWith("file:///")) {
+	    		;//pass
+	    	} else {
+	    		localFileUrl = "file:///" + urlParam.substring(7);
+	    	}
+	    }
+	    
 		String errMsg = null;
 		try {
-			FileObject fo = HyperlinkMenu.getInstance().resolveWebdavFile(temp);
-			if (fo == null) {
-				;//user may have cancelled the authentication challenge
-			} else if (fo.exists()) {
+			FileObject fo = VFSUtils.getFileSystemManager().resolveFile(urlParam);
+			if (fo.exists()) {
 		        Preferences prefs = Preferences.userNodeForPackage(FileMenu.class);
-				String lastFileUri = fo.getName().getURI();
-				prefs.put(Constants.LAST_OPENED_FILE, lastFileUri);
-				if (fo.getName().getScheme().equals(("file"))) {
-					prefs.put(Constants.LAST_OPENED_LOCAL_FILE, lastFileUri);
-				}
+		        localFileUrl = fo.getName().getURI();
+				prefs.put(Constants.LAST_OPENED_FILE, localFileUrl);
+				prefs.put(Constants.LAST_OPENED_LOCAL_FILE, localFileUrl);
 				log.info("\n\n Opening " + urlParam);
 				editor.createInternalFrame(fo);
 			} else {
@@ -129,68 +133,99 @@ public class WordMLApplet extends JApplet {
 			String title = rm.getString("Application.applet.initialisation.text");
 			editor.showMessageDialog(title, errMsg, JOptionPane.ERROR_MESSAGE);
 		}
-	}
+	} //openLocalFile()
 	
-	private String createVFSUrl(String urlParam, String defaultBookmarkEntryTitle) {
-		String theVFSUrl = urlParam;
-		
-		VFSURIParser reference = 
-			findBookmarkEntry(
-				new VFSURIParser(urlParam, false), 
-				defaultBookmarkEntryTitle);
-		if (reference != null
-			&& reference.getUsername() != null
-			&& reference.getUsername().length() > 0
-			&& reference.getPassword() != null
-			&& reference.getPassword().length() > 0) {
-			StringBuilder sb = new StringBuilder();
-			sb.append(reference.getProtocol().getName().toLowerCase());
-			sb.append("://");
-			sb.append(reference.getUsername());
-			sb.append(":");
-			sb.append(reference.getPassword());
-			sb.append("@");
-			int idx = urlParam.indexOf("://") + 3;
-			sb.append(urlParam.substring(idx));
-			theVFSUrl = sb.toString();
+	private void openRemoteFile(WordMLEditor editor, String urlParam) {
+		String webdavUrl = urlParam;
+		if (urlParam.startsWith("http://")) {
+			webdavUrl = "webdav://" + urlParam.substring(7);
 		}
 		
-		return theVFSUrl;
-	}
-	
-	private VFSURIParser findBookmarkEntry(VFSURIParser uri, String defaultBookmarkEntryTitle) {
-		VFSURIParser theEntry = null;
-		
-		Bookmarks book = new Bookmarks();
-		
-		for (int i=0; i < book.getSize(); i++) {
-			TitledURLEntry entry = book.getEntry(i);
-			theEntry = new VFSURIParser(entry.getURL(), false);
-			if (uri.getProtocol() == theEntry.getProtocol()
-				&& uri.getHostname().equals(theEntry.getHostname())
-				&& uri.getPath().startsWith(theEntry.getPath())) {
-				i = book.getSize(); //break
-			} else {
-				theEntry = null;
-			}
+        ResourceMap rm = editor.getContext().getResourceMap(WordMLEditor.class);
+        
+		String temp = 
+			rm.getString(Constants.VFSJFILECHOOSER_DEFAULT_WEBDAV_FOLDER_BOOKMARK_NAME);
+		if (temp == null || temp.length() == 0) {
+			temp = "Default Webdav Folder";
+		} else {
+			temp = temp.trim();
 		}
+		
+		List<String> userCredentials = 
+			AuthenticationUtil.collectUserCredentialsFromBookmark(
+				new VFSURIParser(webdavUrl, false), 
+				temp);
+		temp = webdavUrl.substring(9); //hostname(:port) and path
 
-		//if previous search failed, try to find bookmark entry
-		//that matches defaultEntryTitle parameter
-		if (theEntry == null) {
-			for (int i=0; i < book.getSize(); i++) {
-				TitledURLEntry entry = book.getEntry(i);
-				if (defaultBookmarkEntryTitle.equalsIgnoreCase(entry.getTitle())) {
-					i = book.getSize(); //break
-					theEntry = new VFSURIParser(entry.getURL(), false);
+		//Try each known userCredential to resolve a FileObject
+		FileObject fo = null;
+		for (String uc: userCredentials) {
+			StringBuilder sb = new StringBuilder();
+			sb.append("webdav://");
+			sb.append(uc);
+			sb.append("@");
+			sb.append(temp);
+			try {
+				fo = VFSUtils.getFileSystemManager().resolveFile(sb.toString());
+				if (fo.exists()) {
+					break;
 				} else {
-					entry = null;
+					fo = null;
 				}
+			} catch (FileSystemException exc) {
+				fo = null;
 			}
 		}
 		
-		return theEntry;
-	}
+		if (fo != null) {
+	        Preferences prefs = Preferences.userNodeForPackage(FileMenu.class);
+			String lastFileUri = fo.getName().getURI();
+			prefs.put(Constants.LAST_OPENED_FILE, lastFileUri);
+
+			log.info("\n\n Opening " + webdavUrl);
+			editor.createInternalFrame(fo);
+
+		} else {
+			String title = rm.getString("Application.applet.initialisation.text");
+			String errMsg = null;
+			try {
+				fo = AuthenticationUtil.userAuthenticationChallenge(editor, webdavUrl, title);
+				if (fo == null) {
+					//user may have cancelled the authentication challenge
+					//or unsuccessfully authenticated himself.
+					//Because AuthenticationUtil.userAuthenticationChallenge()
+					//has displayed authentication failure message, we do
+					//not need to do anything here.
+				} else if (fo.exists()) {
+			        Preferences prefs = Preferences.userNodeForPackage(FileMenu.class);
+					String lastFileUri = fo.getName().getURI();
+					prefs.put(Constants.LAST_OPENED_FILE, lastFileUri);
+					if (fo.getName().getScheme().equals(("file"))) {
+						prefs.put(Constants.LAST_OPENED_LOCAL_FILE, lastFileUri);
+					}
+
+					log.info("\n\n Opening " + webdavUrl);
+					editor.createInternalFrame(fo);
+					
+				} else {
+					errMsg = 
+						rm.getString(
+							"Application.applet.file.not.found.message", 
+							urlParam);
+				}
+			} catch (FileSystemException exc) {
+				exc.printStackTrace();
+				errMsg = 
+					rm.getString(
+						"Application.applet.file.io.error.message", 
+						urlParam);
+			}
+			
+			if (errMsg != null) {
+				editor.showMessageDialog(title, errMsg, JOptionPane.ERROR_MESSAGE);
+			}
+		}
+	} //openRemoteFile()
 	
 }// WordMLApplet class
 
