7e191d5eb9cbafaf4b6f33f06d36e9c7b5fd4645
jojada <jojada@e6e0f55d-d614-4f4a-b1a3-5214db3aa930>
2008-12-11 10:37:30 +0000

    An ImpliedParagraphView class.

---
+++ docx4all/src/main/java/org/docx4all/swing/text/ImpliedParagraphView.java
@@ -0,0 +1,85 @@
+/*
+ *  Copyright 2007, Plutext Pty Ltd.
+ *   
+ *  This file is part of Docx4all.
+
+    Docx4all is free software: you can redistribute it and/or modify
+    it under the terms of version 3 of the GNU General Public License 
+    as published by the Free Software Foundation.
+
+    Docx4all is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License   
+    along with Docx4all.  If not, see <http://www.gnu.org/licenses/>.
+    
+ */
+
+package org.docx4all.swing.text;
+
+import javax.swing.text.AttributeSet;
+import javax.swing.text.Element;
+import javax.swing.text.Position;
+import javax.swing.text.View;
+
+import org.apache.log4j.Logger;
+
+public class ImpliedParagraphView extends javax.swing.text.ParagraphView {
+	private static Logger log = Logger.getLogger(ParagraphView.class);
+
+	public ImpliedParagraphView(Element elem) {
+		super(elem);
+		strategy = new FlowStrategy();
+	}
+
+    protected void setParagraphInsets(AttributeSet attr) {
+    	//Do not set insets. They are set by parent view.
+    	//See: org.docx4all.swing.text.ParagraphView
+    }
+    
+	//===== INNER CLASS =====\\
+	class FlowStrategy extends javax.swing.text.FlowView.FlowStrategy {
+		protected View createView(
+			javax.swing.text.FlowView fv,
+			int startOffset,
+			int spanLeft,
+			int rowIndex) {
+
+			// Get the child view that contains the given starting position
+			View lv = getLogicalView(fv);
+			int childIndex =
+				lv.getViewIndex(startOffset, Position.Bias.Forward);
+			View v = lv.getView(childIndex);
+
+			if (v instanceof RunView) {
+				RunView runV = (RunView) v;
+				v = runV.getTextView(startOffset);
+			}
+
+			if (startOffset == v.getStartOffset()) {
+				// return the entire view
+				return v;
+			}
+
+			// return a fragment.
+			v = v.createFragment(startOffset, v.getEndOffset());
+			return v;
+		}
+	}
+
+
+}// ImpliedParagraphView class
+
+
+
+
+
+
+
+
+
+
+
+
