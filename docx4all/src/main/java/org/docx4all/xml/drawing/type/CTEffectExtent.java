e0f8b39521a8b15fd0bc029bfe661274f313f568
jojada <jojada@e6e0f55d-d614-4f4a-b1a3-5214db3aa930>
2008-12-16 18:22:40 +0000

    A CTEffectExtent class.

---
+++ docx4all/src/main/java/org/docx4all/xml/drawing/type/CTEffectExtent.java
@@ -0,0 +1,77 @@
+/*
+ *  Copyright 2007, Plutext Pty Ltd.
+ *   
+ *  This file is part of Docx4all.
+
+    Docx4all is free software: you can redistribute it and/or modify
+    it under the terms of version 3 of the GNU General Public License 
+    as published by the Free Software Foundation.
+
+    Docx4all is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License   
+    along with Docx4all.  If not, see <http://www.gnu.org/licenses/>.
+    
+ */
+
+package org.docx4all.xml.drawing.type;
+
+import org.docx4all.swing.text.StyleSheet;
+
+/**
+ *	@author Jojada Tirtowidjojo - 17/12/2008
+ */
+public class CTEffectExtent {
+	private org.docx4j.dml.CTEffectExtent ctEffectExtent;
+	
+	public CTEffectExtent(org.docx4j.dml.CTEffectExtent ctEffectExtent) {
+		this.ctEffectExtent = ctEffectExtent;
+	}
+	
+	public Object getDocxObject() {
+		return this.ctEffectExtent;
+	}
+	
+	public int getLeftInPixels() {
+		int i = Long.valueOf(ctEffectExtent.getL()).intValue();
+		return StyleSheet.emuToPixels(i);
+	}
+	
+	public int getRightInPixels() {
+		int i = Long.valueOf(ctEffectExtent.getR()).intValue();
+		return StyleSheet.emuToPixels(i);
+	}
+	
+	public int getTopInPixels() {
+		int i = Long.valueOf(ctEffectExtent.getT()).intValue();
+		return StyleSheet.emuToPixels(i);
+	}
+	
+	public int getBottomInPixels() {
+		int i = Long.valueOf(ctEffectExtent.getB()).intValue();
+		return StyleSheet.emuToPixels(i);
+	}
+	
+}// CTEffectExtent class
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
