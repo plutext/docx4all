a4b68372864cdbf5aec799a9a268f0f1d62965f2
jojada <jojada@e6e0f55d-d614-4f4a-b1a3-5214db3aa930>
2009-02-16 01:17:35 +0000

    Added GlyphView.JustificationInfo class, justificationInfo field, and getJustificationInfo() method. This is needed to support numbering layout in ImpliedParagraphView.Row class.

--- docx4all/src/main/java/org/docx4all/swing/text/LabelView.java
+++ docx4all/src/main/java/org/docx4all/swing/text/LabelView.java
@@ -20,8 +20,12 @@
 package org.docx4all.swing.text;
 
 import java.awt.Color;
+import java.util.BitSet;
 
+import javax.swing.text.BadLocationException;
+import javax.swing.text.Document;
 import javax.swing.text.Element;
+import javax.swing.text.Segment;
 
 import org.docx4all.xml.ElementML;
 import org.docx4all.xml.HyperlinkML;
@@ -36,6 +40,7 @@ public class LabelView extends javax.swing.text.LabelView {
 	private boolean impliedUnderline;
 	private boolean impliedStrikethrough;
 	private Color foreground;
+    private JustificationInfo justificationInfo = null;
 	
     public LabelView(Element elem) {
     	super(elem);
@@ -71,6 +76,95 @@ public class LabelView extends javax.swing.text.LabelView {
     	return (foreground == null) ? super.getForeground() : foreground;
     }
     
+    public Segment getTextSegment(int p0, int p1) {
+        Segment text = new Segment();
+        try {
+            Document doc = getDocument();
+            doc.getText(p0, p1 - p0, text);
+        } catch (BadLocationException bl) {
+            throw new IllegalStateException("LabelView: Stale view: " + bl);
+        }
+        return text;
+    }
+
+    JustificationInfo getJustificationInfo(int rowStartOffset) {
+        if (justificationInfo != null) {
+            return justificationInfo;
+        }
+        //states for the parsing
+        final int TRAILING = 0;
+        final int CONTENT  = 1;
+        final int SPACES   = 2;
+        int startOffset = getStartOffset();
+        int endOffset = getEndOffset();
+        Segment segment = getTextSegment(startOffset, endOffset);
+        int txtOffset = segment.offset;
+        int txtEnd = segment.offset + segment.count - 1;
+        int startContentPosition = txtEnd + 1;
+        int endContentPosition = txtOffset - 1;
+        int lastTabPosition = txtOffset - 1;
+        int trailingSpaces = 0;
+        int contentSpaces = 0;
+        int leadingSpaces = 0;
+        boolean hasTab = false;
+        BitSet spaceMap = new BitSet(endOffset - startOffset + 1);
+
+        //we parse conent to the right of the rightmost TAB only.
+        //we are looking for the trailing and leading spaces.
+        //position after the leading spaces (startContentPosition)
+        //position before the trailing spaces (endContentPosition)
+        for (int i = txtEnd, state = TRAILING; i >= txtOffset; i--) {
+            if (' ' == segment.array[i]) {
+                spaceMap.set(i - txtOffset);
+                if (state == TRAILING) {
+                    trailingSpaces++;
+                } else if (state == CONTENT) {
+                    state = SPACES;
+                    leadingSpaces = 1;
+                } else if (state == SPACES) {
+                    leadingSpaces++;
+                }
+            } else if ('\t' == segment.array[i]) {
+                hasTab = true;
+                break;
+            } else {
+                if (state == TRAILING) {
+                    if ('\n' != segment.array[i]
+                          && '\r' != segment.array[i]) {
+                        state = CONTENT;
+                        endContentPosition = i;
+                    }
+                } else if (state == CONTENT) {
+                    //do nothing
+                } else if (state == SPACES) {
+                    contentSpaces += leadingSpaces;
+                    leadingSpaces = 0;
+                }
+                startContentPosition = i;
+            }
+        }
+
+        int startJustifiableContent = -1;
+        if (startContentPosition < txtEnd) {
+            startJustifiableContent = 
+                startContentPosition - txtOffset;
+        }
+        int endJustifiableContent = -1;
+        if (endContentPosition > txtOffset) {
+            endJustifiableContent = 
+                endContentPosition - txtOffset;
+        }
+        justificationInfo = 
+            new JustificationInfo(startJustifiableContent,
+                                  endJustifiableContent,
+                                  leadingSpaces,
+                                  contentSpaces,
+                                  trailingSpaces,
+                                  hasTab,
+                                  spaceMap);
+        return justificationInfo;
+    }
+
     protected void setPropertiesFromAttributes() {
     	super.setPropertiesFromAttributes();
     	if (isImpliedUnderline()) {
@@ -81,6 +175,34 @@ public class LabelView extends javax.swing.text.LabelView {
     	}
     }
     
+    /**
+     * Class to hold data needed to justify this GlyphView in a PargraphView.Row
+     */
+    static class JustificationInfo {
+        //justifiable content start
+        final int start;
+        //justifiable content end
+        final int end;
+        final int leadingSpaces;
+        final int contentSpaces;
+        final int trailingSpaces;
+        final boolean hasTab;
+        final BitSet spaceMap;
+        JustificationInfo(int start, int end,
+                          int leadingSpaces, 
+                          int contentSpaces,
+                          int trailingSpaces,
+                          boolean hasTab,
+                          BitSet spaceMap) {
+            this.start = start;
+            this.end = end;
+            this.leadingSpaces = leadingSpaces;
+            this.contentSpaces = contentSpaces;
+            this.trailingSpaces = trailingSpaces;
+            this.hasTab = hasTab;
+            this.spaceMap = spaceMap;
+        }
+    }
 }// LabelView class
 
 
